bcf601a188ef67c50f8393f52c64a508f263e8db
Baybars <baybarsstanridagli@gmail.com>
2025-01-03 11:59:59 +0300

    Trial

---
+++ Trial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number, result;
+            Console.WriteLine("Please Enter The Number: ");
+            number = double.Parse(Console.ReadLine());
+            result = number * 18 / 100;
+            Console.WriteLine("18 Percent of The Entered Number :{0}",result);
+            Console.ReadKey();
+        }
+    }
+}
